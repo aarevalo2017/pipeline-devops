855619dfee4cd76588a5a33552823dda28ecb6a4
Alejandro Arevalo <aoarevalo@CLFTC-C02G54TWMD6N.local>
2022-02-06 02:37:42 -0300

    update 2

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,6 +1,11 @@
 def call(stages){
   def stagesList = stages.split(";")
   def listStagesOrder = [
+       'compile': 'stageCompile',
+        'test': 'stageTest',
+        'build': 'stageBuild',
+        'sonar': 'stageSonar',
+        'upload_nexus': 'stageUploadNexus',
       'download_nexus': 'stageDownloadNexus',
       'run_jar': 'stageRunJar',
       'curl_jar': 'stageCurlJar',
@@ -25,17 +30,88 @@ return this;
 
 def executeAllStages(){
   echo "Ejecutando todos los stages..."
-  stageDownloadNexus();
-  stageRunJar();
-  stageCurlJar();
+    stageCompile()
+    stageTest()
+    stageBuild()
+    stageSonar()
+    stageUploadNexus()
+  stageDownloadNexus()
+    stageRunJar()
+  stageCurlJar()
+}
+def stageCompile() {
+    env.DESCRIPTION_STAGE = "Paso 1: Compilar"
+    stage("${env.DESCRIPTION_STAGE}"){
+        env.STAGE = "compile - ${env.DESCRIPTION_STAGE}"
+        sh "echo  ${env.STAGE}"
+        sh "mvn clean compile -e"
+    }
 }
 
+def stageTest() {
+    env.DESCRIPTION_STAGE = "Paso 2: Testear"
+    stage("${env.DESCRIPTION_STAGE}"){
+        env.STAGE = "test - ${env.DESCRIPTION_STAGE}"
+        sh "echo  ${env.STAGE}"
+        sh "mvn clean test -e"
+    }
+}
+
+def stageBuild() {
+    env.DESCRIPTION_STAGE = "Paso 3: Build jar"
+    stage("${env.DESCRIPTION_STAGE}"){
+        env.STAGE = "build - ${env.DESCRIPTION_STAGE}"
+        sh "echo  ${env.STAGE}"
+        sh "mvn clean package -e"
+    }
+}
+
+def stageSonar() {
+    env.DESCRIPTION_STAGE = "Paso 4: Análisis SonarQube"
+    stage("${env.DESCRIPTION_STAGE}"){
+        env.STAGE = "sonar - ${env.DESCRIPTION_STAGE}"
+        repoName = env.GIT_URL.tokenize('/')[3].split("\\.")[0]
+        println(repoName)
+        withSonarQubeEnv('sonarqube') {
+            sh "echo  ${env.STAGE}"
+            def sonarName = "${repoName}-${env.BRANCH_NAME}-${env.BUILD_NUMBER}"
+            println(sonarName)
+            sh 'mvn clean verify sonar:sonar -Dsonar.projectKey=github-sonar -Dsonar.projectName=' + sonarName 
+        }
+    }
+}
+
+def stageUploadNexus() {
+  env.DESCRIPTION_STAGE = "Paso 5: Subir Nexus"
+  stage("${env.DESCRIPTION_STAGE}"){
+        env.STAGE = "upload_nexus - ${env.DESCRIPTION_STAGE}"
+        sh "echo  ${env.STAGE}"
+        nexusPublisher nexusInstanceId: 'nexus',
+        nexusRepositoryId: 'devops-laboratorio',
+        packages: [
+            [$class: 'MavenPackage',
+                mavenAssetList: [
+                    [classifier: '',
+                    extension: 'jar',
+                    filePath: "build/DevOpsUsach2020-0.1.2.jar"
+                ]
+            ],
+                mavenCoordinate: [
+                    artifactId: 'DevOpsUsach2020',
+                    groupId: 'com.devopsusach2020',
+                    packaging: 'jar',
+                    version: "0.1.2"
+                ]
+            ]
+        ]
+    }
+}
 def stageDownloadNexus(){
     env.DESCRIPTION_STAGE = "Paso 1: Descargar Nexus"
     stage("${env.DESCRIPTION_STAGE}"){
       env.STAGE = "download_nexus - ${env.DESCRIPTION_STAGE}"
       sh "echo  ${env.STAGE}"
-      sh "curl -X GET -u $NEXUS_USERNAME:$NEXUS_PASSWORD 'http://nexus:8081/repository/devops-laboratorio/com/devopsusach2020/DevOpsUsach2020/${env.POM_VERSION}/DevOpsUsach2020-${env.POM_VERSION}.jar' -O"
+      sh "curl -X GET -u $NEXUS_USERNAME:$NEXUS_PASSWORD 'http://nexus:8081/repository/devops-laboratorio/com/devopsusach2020/DevOpsUsach2020/0.1.2/DevOpsUsach2020-0.1.2.jar' -O"
     }
 }
 
@@ -44,7 +120,7 @@ def stageRunJar(){
     stage("${env.DESCRIPTION_STAGE}"){
       env.STAGE = "run_jar - ${env.DESCRIPTION_STAGE}"
       sh "echo  ${env.STAGE}"
-      sh "nohup java -jar DevOpsUsach2020-${env.POM_VERSION}.jar & >/dev/null"
+      sh "nohup java -jar DevOpsUsach2020-0.1.2.jar & >/dev/null"
   }
 }
 
